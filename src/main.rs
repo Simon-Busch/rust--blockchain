c8451408c12edd1c6a8ef0c6cd53f9154ab35630
simon busch <simon.busch@live.fr>
2023-11-18 06:37:18 +0100

    add transactions

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,17 @@ use crate::blockchain::Blockchain;
 
 mod block;
 mod blockchain;
+mod transactions;
 
 fn main() {
     println!("Hello, world!");
     let mut oxsi_coin = Blockchain::new();
     oxsi_coin.add_block("Block 1 Data".to_owned());
     oxsi_coin.add_block("Block 2 Data".to_owned());
+    oxsi_coin.add_block("Block 3 Data".to_owned());
+    oxsi_coin.add_block("Block 4 Data".to_owned());
+    oxsi_coin.add_block("Block 5 Data".to_owned());
+    oxsi_coin.add_block("Block 6 Data".to_owned());
     println!("{:#?}", oxsi_coin);
 
 }
---
+++ src/transactions.rs
@@ -0,0 +1,42 @@
+use secp256k1::{Message, PublicKey, Secp256k1, SecretKey, Signature};
+use std::str::FromStr;
+
+pub struct Transaction {
+    pub sender: String,
+    pub recipient: String,
+    pub amount: f32,
+    pub signature: String,
+}
+
+impl Transaction {
+    // Assume `self` has the sender, recipient, and amount fields populated.
+    pub fn sign(&mut self, secret_key: &SecretKey) {
+        let context = Secp256k1::new();
+        let message = self.create_message();
+        let signature = context.sign(&Message::from_slice(&message).unwrap(), secret_key);
+
+        self.signature = signature.to_string();
+    }
+    pub fn create_message(&self) -> Vec<u8> {
+        let mut bytes = Vec::new();
+        bytes.extend(self.sender.as_bytes());
+        bytes.extend(self.recipient.as_bytes());
+        bytes.extend(&self.amount.to_le_bytes());
+        bytes
+    }
+
+    // Verify that the signature is correct
+    pub fn verify(&self, public_key: &PublicKey) -> bool {
+        let context = Secp256k1::new();
+        let message = self.create_message();
+        let signature = Signature::from_str(&self.signature).unwrap();
+
+        context
+            .verify(
+                &Message::from_slice(&message).unwrap(),
+                &signature,
+                public_key,
+            )
+            .is_ok()
+    }
+}
